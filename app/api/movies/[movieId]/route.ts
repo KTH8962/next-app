c61321037fe489a90b1418ad017ed30239d10f7a
KTH8962 <gaya8962@gmail.com>
2025-01-30 21:36:29 +0900

    버전수정

--- app/api/movies/[movieId]/route.ts
+++ app/api/movies/[movieId]/route.ts
@@ -1,14 +1,17 @@
-import type { NextRequest } from "next/server"
+import { NextRequest } from "next/server"
 
-type Context = {
-  params: { movieId: string }
-}
+export async function GET(request: NextRequest) {
+  // request.nextUrl.pathname에서 동적 경로 매개변수(movieId) 추출
+  const movieId = request.nextUrl.pathname.split("/").pop()
 
-export async function GET(request: NextRequest, context: Context) {
-  const { movieId } = context.params // 동적 경로
+  if (!movieId) {
+    return new Response("Movie ID is missing", { status: 400 })
+  }
+
   const res = await fetch(
     `https://omdbapi.com/?apikey=${process.env.NEXT_API_KEY}&i=${movieId}`
   )
   const data = await res.json()
+
   return Response.json(data)
 }
